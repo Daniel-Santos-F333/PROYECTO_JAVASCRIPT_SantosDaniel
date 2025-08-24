83758be0a34d89cc732b10d924f69943cf9f7633
Daniel <cuentaanormal1208@gmail.com>
2025-08-24 03:04:46 -0500

    feat: :fire: replace script and test link in login.js

---
+++ scripts/login.js
@@ -0,0 +1,6 @@
+window.addEventListener("DOMContentLoaded", () => {
+  console.log("Página cargada");
+
+  document.getElementById("username").value = "";
+  document.getElementById("password").value = "";
+});
